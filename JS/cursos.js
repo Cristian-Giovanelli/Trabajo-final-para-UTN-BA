c86402a4bb93e914335d0f62f3ab3b81c19f97ac
Cristian-Giovanelli <cristiangiovanelligmail.com>
2023-06-09 17:32:42 -0300

    Primer COMMIT

---
+++ JS/cursos.js
@@ -0,0 +1,5 @@
+function InvitacionCurso() {
+    alert("Si estás aquí, seguro te puede interesar nuestra FORMACIÓN. INSCRIBITE, sin cargo en nuestro WEBINAR para hidroponistas domésticos");
+  }
+  
+  setTimeout(InvitacionCurso, 5000);
